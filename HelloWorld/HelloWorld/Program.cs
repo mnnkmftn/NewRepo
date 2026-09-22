d773ae9ea3cfd3f4a46a4693b9f9cebf94b74832
алиса <nikanikiforova1776@gmail.com>
2026-09-22 13:56:33 +0500

    Вывод цветного текста в консоль

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Hello, World!");
+            Console.ResetColor();
+
+            //Console.ReadKey();
+        }
+    }
+}
