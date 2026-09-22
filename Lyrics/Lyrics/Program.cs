b0f7e49c4b3b10ba55e758b50221e645e1fafb4f
алиса <nikanikiforova1776@gmail.com>
2026-09-22 15:03:05 +0500

    вывод разноцветного стихотворения в консоль

---
+++ Lyrics/Lyrics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyrics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Юрий Левитанский"+"\r\nКинематограф"+"\r\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Это город. Еще рано. Полусумрак, полусвет.");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("А потом на крышах солнце, а на стенах еще нет.");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("А потом в стене внезапно загорается окно.");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Возникает звук рояля. Начинается кино." + "\r\n");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("И очнулся, и качнулся, завертелся шар земной.");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Ах, механик, ради бога, что ты делаешь со мной!");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Этот луч, прямой и резкий, эта света полоса");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("заставляет меня плакать и смеяться два часа, ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("быть участником событий, пить, любить, идти на дно..." + "\r\n");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Жизнь моя, кинематограф, черно-белое кино!" + "\r\n");
+
+            Console.ResetColor();
+        }
+    }
+}
